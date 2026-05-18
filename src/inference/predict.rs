2b29db9b2858dfdb856d070e276c569c47ac8077
SauersML <123714540+SauersML@users.noreply.github.com>
2026-05-18 09:03:56 -0500

    Lock down covariance inference contract

--- src/inference/predict.rs
+++ src/inference/predict.rs
@@ -6127,6 +6127,10 @@ mod tests {
             beta_standard_errors: None,
             beta_covariance_corrected: None,
             beta_standard_errors_corrected: None,
+            beta_covariance_frequentist: None,
+            influence_matrix: None,
+            dispersion: crate::estimate::Dispersion::Known(1.0),
+            covariance_is_diagonal_only: false,
             bias_correction_beta,
         };
         UnifiedFitResult::new_for_test_unchecked(UnifiedFitResultParts {
