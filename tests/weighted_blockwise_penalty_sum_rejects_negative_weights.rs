c9b39a74b387df591d2357e82837efa1c70e64d7
SauersML <123714540+SauersML@users.noreply.github.com>
2026-05-24 23:50:22 -0500

    [bug-hunt] Find bugs in smooth specifications

---
+++ tests/weighted_blockwise_penalty_sum_rejects_negative_weights.rs
@@ -0,0 +1,9 @@
+use gam::terms::smooth::{weighted_blockwise_penalty_sum, BlockwisePenalty};
+use ndarray::array;
+
+#[test]
+#[should_panic(expected = "negative")]
+fn bug_weighted_blockwise_penalty_sum_accepts_negative_weight_instead_of_erroring() {
+    let penalties = vec![BlockwisePenalty::new(0..2, array![[2.0, 0.0], [0.0, 3.0]])];
+    let _result = weighted_blockwise_penalty_sum(&penalties, &[-1.0], 2);
+}
