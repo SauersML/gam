46eb8cb52cb875af840dea408126ddbca43a779e
SauersML <123714540+SauersML@users.noreply.github.com>
2026-05-24 23:49:43 -0500

    [bug-hunt] Add tests for log-kappa derivatives bugs

---
+++ tests/basis_duchon_anisotropic_dominant_axis_factorization.rs
@@ -0,0 +1,10 @@
+use gam::terms::basis::{build_duchon_basis, CenterStrategy, DuchonBasisSpec, DuchonNullspaceOrder};
+use ndarray::Array2;
+
+#[test]
+fn anisotropic_build_handles_dominant_axis_without_instability() {
+    let data = Array2::from_shape_vec((5, 3), vec![0.0,0.0,0.0, 0.4,0.2,0.1, 0.8,0.5,0.2, 1.2,0.7,0.3, 1.6,1.0,0.5]).unwrap();
+    let spec = DuchonBasisSpec { center_strategy: CenterStrategy::UserProvided(data.clone()), periodic: None, length_scale: Some(1.0), power: 2.0, nullspace_order: DuchonNullspaceOrder::Linear, identifiability: Default::default(), aniso_log_scales: Some(vec![6.0, -3.0, -3.0]), operator_penalties: Default::default(), boundary: Default::default() };
+    let b = build_duchon_basis(data.view(), &spec).unwrap().design.to_dense();
+    assert!(b.iter().all(|v| v.is_finite()), "anisotropic build should remain finite when one axis dominates strongly");
+}
