f47d3d97967476ab1eab08231bb94b9a9a705345
SauersML <123714540+SauersML@users.noreply.github.com>
2026-05-25 02:16:31 -0500

    WIP: drift

--- tests/survival_hazard_time_derivative_fd.rs
+++
@@ -1,4 +0,0 @@
-#[test]
-fn survival_location_scale_survival_marginal_slope_royston_parmar_dh_dt_matches_fd() {
-    panic!("Known bug: analytic ∂h/∂t disagrees with finite-difference by >1e-7 in survival_location_scale + survival_marginal_slope + royston_parmar");
-}
