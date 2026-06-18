34ae2efd29bc7fdbf580c9d7d2532eab4dcb3c86
SauersML <scottsauersc@gmail.com>
2026-06-18 15:57:50 -0500

    Fix tensor tuple k parsing #1279

--- src/inference/formula_dsl.rs
+++ src/inference/formula_dsl.rs
@@ -33,7 +33,7 @@ unary_op = _{ "+" | "-" }
 
 primary = { function_call | list_lit | tuple_lit | ident | number | string_lit | "(" ~ expr ~ ")" }
 list_lit = @{ "[" ~ (!"]" ~ ANY)* ~ "]" }
-tuple_lit = @{ "(" ~ (!")" ~ ANY)* ~ "," ~ (!")" ~ ANY)* ~ ")" }
+tuple_lit = @{ "(" ~ (!("," | ")") ~ ANY)+ ~ "," ~ (!")" ~ ANY)* ~ ")" }
 function_call = { ident ~ "(" ~ arg_list? ~ ")" }
 arg_list = { arg ~ ("," ~ arg)* }
 arg = { named_arg | expr }
